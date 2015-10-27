03e38dbcc6efc8a35b63692e8c29a5503a02370e
Bjarke Staun-Olsen <bjarkestaun@gmail.com>
2015-10-27 16:51:44 -0700

    added asking for username on launch

--- client/env/config.js
+++ client/env/config.js
@@ -3,7 +3,6 @@
 // All this is doing is inserting the parse API keys into every $.ajax
 // request that you make so you don't have to.
 if (!/(&|\?)username=/.test(window.location.search)) {
-  debugger;
   var newSearch = window.location.search;
   if (newSearch !== '' & newSearch !== '?') {
     newSearch += '&';
